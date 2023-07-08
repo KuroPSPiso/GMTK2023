b9a65c05fa3a1f10941122964d793ece013ee37e
Ryan van den Bogaard <bogaardryan@hotmail.com>
2023-07-08 07:41:05 +0200

    Screen orientation + player defaults

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 #pragma warning disable CS0414
 public class PlayerMovement : MonoBehaviour
 {
-    public float maxSpeed = 5f;
+    public float fireRate = 1f;
+    public int powerUpStage = 1;
+    public int score = 0;
+    public float movementSpeed = 5f;
+    public float rangeMovement = 2f;
+
+    public bool IsDebug = false;
+
     int threatLimit = 10;
     float rayDist = 40f;
-
-
     int threats = 0;
 
+    void Move()
+    {
+        if (this.IsDebug)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (this.transform.position.x > this.rangeMovement * -1f)
+                    this.transform.position -= new Vector3(movementSpeed * Time.fixedDeltaTime, 0f);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if (this.transform.position.x < this.rangeMovement)
+                    this.transform.position += new Vector3(movementSpeed * Time.fixedDeltaTime, 0f);
+            }
+        }
+    }
 
     private void FixedUpdate()
     {
         RaycastHit hitInfo;
-        if(Physics.Raycast(new Ray(this.transform.position, this.transform.forward), out hitInfo, this.rayDist))
+        if(Physics.Raycast(new Ray(this.transform.position, this.transform.position + this.transform.up), out hitInfo, this.rayDist))
         {
 
         }
+
+        Move();
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(this.transform.position, this.transform.forward * this.rayDist);
+        Color c = Gizmos.color;
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(this.transform.position, this.transform.position + this.transform.up * this.rayDist);
+        Gizmos.DrawLine(Vector3.zero, this.transform.right * this.rangeMovement);
+        Gizmos.DrawLine(Vector3.zero, -this.transform.right * this.rangeMovement);
+
+        Gizmos.color = c;
     }
 }
